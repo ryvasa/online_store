77d94e505b1e80743c3c9e1836881618962f6bff
ryvasa <ryanoktaviansaputra9@gmail.com>
2023-03-29 08:53:59 +0700

    update realtime chat

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,22 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { MdSend } from "react-icons/md";
-import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 import { useState } from "react";
 import axios from "axios";
 import { refreshToken } from "../utils/refreshToken";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/id";
 import { Link, useParams } from "react-router-dom";
-import { useRef } from "react";
-
+import { io } from "socket.io-client";
 const Chat = () => {
+  const socket = useRef();
   const user = useSelector((state) => state.user.currentUser);
   const [chats, setChats] = useState([]);
   const [messages, setMessages] = useState([]);
+  const [receivedMessage, setReceivedMessage] = useState([]);
   const [message, setMessage] = useState("");
   const { id } = useParams();
+  const [room, setRoom] = useState({});
   const scrollRef = useRef(null);
   useEffect(() => {
     scrollRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -25,19 +26,37 @@ const Chat = () => {
     refreshToken().then(() => {
       getAllChats();
     });
-  }, []);
+  }, [messages]);
+  useEffect(() => {
+    receivedMessage && setMessages((prev) => [...prev, receivedMessage]);
+  }, [receivedMessage]);
+
   useEffect(() => {
     if (id) {
-      refreshToken().then(() => {
-        getChatMessages();
+      setRoom({ id: id, user: user.uuid });
+      socket.current = io("http://localhost:8000");
+
+      socket.current.on("receive_message", (data) => {
+        setReceivedMessage(data);
       });
     }
+  }, [id, socket]);
+
+  useEffect(() => {
+    refreshToken().then(() => {
+      getChatMessages();
+    });
   }, [id]);
+
+  useEffect(() => {
+    if (room && id) {
+      joinRoom();
+    }
+  }, [room]);
   const getAllChats = async () => {
     try {
       const response = await axios.get("http://localhost:5000/chats/all");
       setChats(response.data);
-      console.log(response);
     } catch (error) {
       console.log(error);
     }
@@ -48,106 +67,131 @@ const Chat = () => {
         `http://localhost:5000/chats/admin/${id}`
       );
       setMessages(response.data.message);
-      console.log(response);
     } catch (error) {
       console.log(error);
     }
   };
   const sendMessage = async () => {
     try {
-      const response = await axios.post("http://localhost:5000/messages", {
-        message,
-        chat_id: id,
-      });
-      console.log(response);
+      const messageData = {
+        user_id: user.uuid,
+        user: {
+          img: user.img,
+          name: user.name,
+        },
+        message: message,
+        createdAt: Date.now(),
+      };
+      await socket.current.emit("send_message", messageData);
+      const response = await axios
+        .post("http://localhost:5000/messages", {
+          message,
+          chat_id: id,
+        })
+        .then(() => {
+          setMessage("");
+        });
+      setMessages((message) => [...message, messageData]);
     } catch (error) {
       console.log(error);
     }
   };
   const handleSubmit = (e) => {
     refreshToken().then(() => {
-      sendMessage().then(() => {
-        setMessage("");
-        getChatMessages();
-      });
+      sendMessage();
     });
   };
-  dayjs.extend(relativeTime);
+  const joinRoom = () => {
+    if (user.name !== "" && room !== "") {
+      socket.current.emit("join_room", room);
+    }
+  };
+  const currentDate = dayjs();
+  const time = (createdAt) => {
+    const createdDate = dayjs(createdAt);
+    if (currentDate.diff(createdDate, "day") < 1) {
+      const formattedTime = createdDate.format("HH:mm");
+      return formattedTime;
+    } else {
+      const formattedDate = createdDate.format("DD MMMM YYYY");
+      return formattedDate;
+    }
+  };
   return (
     <Layout>
       <div className="flex-[5] overflow-y-auto h-screen pb-16 w-full">
         <div className="flex w-full h-full">
           <div className="flex-1 border-r  bg-gray-100">
             <div className="overflow-y-scroll h-screen">
-              {chats.map((chat) =>
-                chat.uuid === id ? (
-                  <Link
-                    to={`/chat/${chat.uuid}`}
-                    key={chat.uuid}
-                    className="border-b px-5 py-2 flex w-full bg-indigo-600 justify-between rounded-md"
-                  >
-                    <div className="flex gap-2 items-center justify-center">
-                      <img
-                        src={
-                          chat.user?.img
-                            ? chat.user?.img
-                            : "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
-                        }
-                        className="w-10 h-10 object-cover rounded-full border-2"
-                        alt=""
-                      />
-                      <div className="flex flex-col">
-                        <span className="text-sm font-semibold text-white ">
-                          {chat.user?.name}
-                        </span>
-                        <span className="text-sm  text-white ">
-                          {chat.message[0]?.message}
-                        </span>
+              {chats.map((chat) => (
+                <div key={chat.uuid}>
+                  {chat.uuid === id ? (
+                    <Link
+                      to={`/chat/${chat.uuid}`}
+                      className="border-b px-5 py-2 flex w-full bg-indigo-600 justify-between rounded-md"
+                    >
+                      <div className="flex gap-2 items-center justify-center">
+                        <img
+                          src={
+                            chat.user?.img
+                              ? chat.user?.img
+                              : "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                          }
+                          className="w-10 h-10 object-cover rounded-full border-2"
+                          alt=""
+                        />
+                        <div className="flex flex-col">
+                          <span className="text-sm font-semibold text-white ">
+                            {chat.user?.name}
+                          </span>
+                          <span className="text-sm  text-white ">
+                            {chat.message[0]?.message}
+                          </span>
+                        </div>
                       </div>
-                    </div>
-                    <div className="flex justify-center items-center">
-                      <div className="flex justify-center items-center gap-1">
-                        <span className=" text-sm text-white ">
-                          {dayjs(chat.message[0]?.createdAt).fromNow()}
-                        </span>{" "}
+                      <div className="flex justify-center items-center">
+                        <div className="flex justify-center items-center gap-1">
+                          <span className=" text-sm text-white ">
+                            {time(chat.message[0]?.createdAt)}
+                          </span>{" "}
+                        </div>
                       </div>
-                    </div>
-                  </Link>
-                ) : (
-                  <Link
-                    to={`/chat/${chat.uuid}`}
-                    key={chat.uuid}
-                    className="border-b px-5 py-2 flex w-full justify-between"
-                  >
-                    <div className="flex gap-2 items-center justify-center">
-                      <img
-                        src={
-                          chat.user?.img
-                            ? chat.user?.img
-                            : "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
-                        }
-                        className="w-10 h-10 object-cover rounded-full border-2"
-                        alt=""
-                      />
-                      <div className="flex flex-col">
-                        <span className="text-sm font-semibold">
-                          {chat.user?.name}
-                        </span>
-                        <span className="text-sm font-light text-indigo-600">
-                          {chat.message[0]?.message}
-                        </span>
+                    </Link>
+                  ) : (
+                    <Link
+                      to={`/chat/${chat.uuid}`}
+                      className="border-b px-5 py-2 flex w-full justify-between"
+                    >
+                      <div className="flex gap-2 items-center justify-center">
+                        <img
+                          src={
+                            chat.user?.img
+                              ? chat.user?.img
+                              : "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                          }
+                          className="w-10 h-10 object-cover rounded-full border-2"
+                          alt=""
+                        />
+                        <div className="flex flex-col">
+                          <span className="text-sm font-semibold">
+                            {chat.user?.name}
+                          </span>
+                          <span className="text-sm font-light text-indigo-600">
+                            {chat.message[0]?.message}
+                          </span>
+                        </div>
                       </div>
-                    </div>
-                    <div className="flex justify-center items-center">
-                      <div className="flex justify-center items-center gap-1">
-                        <span className="font-light text-sm text-indigo-600">
-                          {dayjs(chat.message[0]?.createdAt).fromNow()}
-                        </span>{" "}
+                      <div className="flex justify-center items-center">
+                        <div className="flex justify-center items-center gap-1">
+                          <span className="font-light text-sm text-indigo-600">
+                            {time(chat.message[0]?.createdAt)}
+                          </span>{" "}
+                        </div>
                       </div>
-                    </div>
-                  </Link>
-                )
-              )}
+                    </Link>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
 
@@ -156,59 +200,61 @@ const Chat = () => {
               <div className="flex-1 overflow-y-auto ">
                 <div className="h-full p-3">
                   <div className="flow-root">
-                    {messages.map((message) =>
-                      message.user_id === user?.uuid ? (
-                        <div className="chat chat-end" key={message.uuid}>
-                          <div className="chat-image avatar">
-                            <div className="w-10 rounded-full">
-                              <img
-                                src={
-                                  message.user?.img
-                                    ? message.user?.img
-                                    : "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
-                                }
-                              />
+                    {messages.map((message, index) => (
+                      <div key={index}>
+                        {message.user_id === user?.uuid ? (
+                          <div className="chat chat-end">
+                            <div className="chat-image avatar">
+                              <div className="w-10 rounded-full">
+                                <img
+                                  src={
+                                    message.user?.img
+                                      ? message.user?.img
+                                      : "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                                  }
+                                />
+                              </div>
+                            </div>
+                            <div className="chat-header">
+                              {message.user?.name}
+                            </div>
+                            <div className="chat-bubble  bg-indigo-600 text-white">
+                              {message.message}
+                            </div>
+                            <div className="chat-footer">
+                              <time className="text-xs opacity-50">
+                                {time(message.createdAt)}
+                              </time>
                             </div>
                           </div>
-                          <div className="chat-header">
-                            {message.user?.name}
-                          </div>
-                          <div className="chat-bubble  bg-indigo-600 text-white">
-                            {message.message}
-                          </div>
-                          <div className="chat-footer">
-                            <time className="text-xs opacity-50">
-                              {dayjs(message.createdAt).fromNow()}
-                            </time>
-                          </div>
-                        </div>
-                      ) : (
-                        <div className="chat chat-start ">
-                          <div className="chat-image avatar">
-                            <div className="w-10 rounded-full">
-                              <img
-                                src={
-                                  message.user?.img
-                                    ? message.user?.img
-                                    : "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
-                                }
-                              />
+                        ) : (
+                          <div className="chat chat-start ">
+                            <div className="chat-image avatar">
+                              <div className="w-10 rounded-full">
+                                <img
+                                  src={
+                                    message.user?.img
+                                      ? message.user?.img
+                                      : "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+                                  }
+                                />
+                              </div>
+                            </div>
+                            <div className="chat-header">
+                              {message.user?.name}
+                            </div>
+                            <div className="chat-bubble bg-gray-400 text-white">
+                              {message.message}
+                            </div>
+                            <div className="chat-footer">
+                              <time className="text-xs opacity-50">
+                                {time(message.createdAt)}
+                              </time>
                             </div>
                           </div>
-                          <div className="chat-header">
-                            {message.user?.name}
-                          </div>
-                          <div className="chat-bubble bg-gray-400 text-white">
-                            {message.message}
-                          </div>
-                          <div className="chat-footer">
-                            <time className="text-xs opacity-50">
-                              {dayjs(message.createdAt).fromNow()}
-                            </time>
-                          </div>
-                        </div>
-                      )
-                    )}
+                        )}
+                      </div>
+                    ))}
                     <div ref={scrollRef} />
                   </div>
                 </div>
@@ -221,6 +267,9 @@ const Chat = () => {
                   placeholder="Type here"
                   className="inline-none rounded-lg w-full border input-md"
                   value={message}
+                  onKeyPress={(event) => {
+                    event.key === "Enter" && sendMessage();
+                  }}
                 />
                 <button onClick={handleSubmit}>
                   <MdSend className="w-6 h-6 text-white" />
